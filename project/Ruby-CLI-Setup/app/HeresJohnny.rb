fbb748b3456b9eb20b47530b4de82e984a8a3d31
nrepasy <nrepasy@gmail.com>
2021-01-05 18:36:47 -0500

    SEcond Day End

--- project/Ruby-CLI-Setup/app/HeresJohnny.rb
+++ project/Ruby-CLI-Setup/app/HeresJohnny.rb
@@ -1,6 +1,11 @@
 class HeresJohnny
+  attr_reader :prompt
+  attr_accessor
   # here will be your CLI!
   # it is not an AR class so you need to add attr
+def  initialize
+@prompt = TTY::Prompt.new
+end
 
   def run
      welcome
@@ -19,10 +24,8 @@ class HeresJohnny
   end
 
   def login_or_signup
-    puts "What is your username?"
-    user = get_input
+    user = prompt.ask("What is your username?")
     @user = User.find_or_create_by(name: user)
-    
   end
 
   def get_input
@@ -30,107 +33,106 @@ class HeresJohnny
      gets.chomp
   end
 
+  def main_menu
+    options = ["Yes, please find me a restroom." , "No, I would like to see my reviews." , "Nah I'm good."]
+    prompt.select("Main Menu:") do |menu|
+
+      menu.choice "Find a restroom.",->{locate_user}
+      menu.choice "My reviews.",->{check_reviews}
+      menu.choice  "Exit.",->{exit}
+    end
+  end
+
   def gotta_go
-    puts "Do you gotta go right now?"
-    input = get_input
-    
-      if input == "yes"
-        locate_user
-      elsif input == "no"
-        check_reviews
-      else
-        puts "please answer yes or no"
-        gotta_go
+  options = ["Yes, please find me a restroom." , "No, I would like to see my reviews." , "Nah I'm good."]
+    prompt.select("Do you gotta go right now?") do |menu|
+
+      menu.choice "Yes, please find me a restroom.",->{locate_user}
+      menu.choice "No, take me to the main menu",->{main_menu}
+      menu.choice  "Nah I'm good.",->{exit}
       end
   end
 
   def locate_user
-    puts "where are you right now?"
-    puts Area.all.pluck(:name)
-    input = get_input
+    input = prompt.select("where are you right now?", Area.all.pluck(:name)) 
     area = Area.find_by(name: input)
-    #binding.pry
     restrooms = Restroom.where(area_id: area.id).pluck(:address)
-    #add rating to restroom output
     puts "There are available restrooms at the following locations"
-    sleep(1)
-    puts restrooms
-    puts "Which would you like to visit?"
-    restroom = get_input
-    # binding.pry
-    get_restroom_instance(restroom)
+    restroom =   prompt.select("Which would you like to visit?", restrooms)
+    # get_restroom_instance(restroom)
     @used_restroom = get_restroom_instance(restroom)
     puts "when you gotta go you gotta go! so go!!!"
     4.times do 
       sleep(1)
       puts "💩"
     end
-  
-    sleep(1)
-    puts "Hope you enjoyed, what would you like to do next?"
-    sleep(1)
-    puts "leave a review?(1)"
-    sleep(1)
-    puts "check out your reviews?(2)"
-    sleep(1)
-    puts "exit?(3)"
-    input = get_input
-    if input == "1"
-      puts "1"
+   sleep(1)
+    satisfied = prompt.yes?("Hope you enjoyed, would you like to leave a review?")
+    if satisfied 
       review
-    elsif input == "2"
-      puts "2"
-      check_reviews
-    elsif input == "3"
-      puts "3"
-      exit!
-    end
-
-  end
-
-  def review
-  puts "on a scale of 1-5, how would you rate this restroom?"
-  new_rating = get_input.to_i
-  # binding.pry
-  Review.create(user_id: @user.id, restroom_id: @used_restroom.id, rating: new_rating) 
-  puts "Thank you for you're feedback!"
-  puts "Would you like to see all of your reviews?"
-  input = get_input
-  if input == "yes"
-    check_reviews
     else 
-      puts "Goodbye!!"
-      exit!
+  puts "Okay, returning to main menu!"
+  sleep(1)
+  main_menu
+    end
   end
+
+def review
+  new_rating = review_helper
+  new_review = Review.find_or_create_by(user_id: @user.id, restroom_id: @used_restroom.id)
+  new_review.update(rating: new_rating) 
+  puts "Thank you for you're feedback!"
+  sleep(1)
+  main_menu
 end
 
   def check_reviews
     # puts @user.restrooms.pluck (:address)
-    # binding.pry
+   
     addy = @user.reviews.map{|x|Restroom.where(id: x.restroom_id).pluck(:address)}.flatten
     rate = @user.reviews.pluck (:rating)
-    # binding.pry
+    
     
     listicle = addy.zip rate
     listicle.each{|x|puts"You gave #{x[0]} a rating of  #{x[1]}!"}
-    update_review 
+
+    prompt.select("Reviews:")  do |menu|
+
+    menu.choice "Update Review",->{update_review}
+    menu.choice "Delete Review",->{delete_review}
+    menu.choice  "Return to Menu",->{main_menu}
+    end
+
+  
     
     # Uses a hash
     # listicle = Hash[addy.zip rate]
     # listicle.each{|k,v| puts "You gave #{k} a rating of #{v}!"}
   end
 
+  def review_helper
+    puts "on a scale of 1-5, how would you rate this restroom?"
+    #FIX POOP EMOJI
+  prompt.slider("stars", min:1, max:5, step:1, symbols:{bullet:"poop emoji", line:"_"}) 
+  end
+
   def update_review
-    puts "Please type a restaurant to review"
-    restroom = get_input
+    restroom = prompt.select("Please select a restaurant to review", @user.restrooms.pluck(:address))
+
+   
     restroom_instance = get_restroom_instance(restroom)
-    puts "please rate from 1-5"
-    rating = get_input.to_i
+    rating = review_helper
     reviewed = Review.find_by(user_id: @user.id, restroom_id: restroom_instance.id)
-    #binding.pry
     reviewed.update(rating: rating)
     puts "Thank you for you're feedback! Your #{restroom} rating has been updated to #{rating}!"
-    delete_review
+    delete = prompt.yes?("Would you like to delete a review?")
+    if delete 
+      delete_review
+    else 
+  puts "Okay, returning to main menu!"
+  sleep(1)
+  main_menu
+    end
   end
 
   def get_restroom_instance(restroom)
